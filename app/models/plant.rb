35aa168678021839ae3a45117f080da08ddec9c5
Caio Andrade <caioertai@gmail.com>
2018-10-25 17:54:15 -0500

    Created seed for tags

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,7 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :garden # adds a presence validation on the garden_id foreign key
 
-  has_many :plant_tags
+  has_many :plant_tags, dependent: :destroy
   has_many :tags, through: :plant_tags
 
   validates :name, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,30 @@ other = Garden.create!(
   banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
 )
 
+tag_names = %w[conífera angiosperma gimnosperma frutífera monocotiledônea bicotiledônea]
+tags = []
+
+tag_names.each do |tag_name|
+  tags << Tag.create!(name: tag_name)
+end
+
 Plant.create!(
   name: "Monstera",
   image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg",
-  garden: little
+  garden: little,
+  tags: tags.sample(2)
 )
 
 Plant.create!(
   name: "Philo",
   image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/philo.jpg",
-  garden: little
+  garden: little,
+  tags: tags.sample(2)
 )
 
 Plant.create!(
   name: "Dieff",
   image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/dieffenbachia.jpg",
-  garden: other
+  garden: other,
+  tags: tags.sample(2)
 )
