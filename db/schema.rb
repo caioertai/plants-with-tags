b081ccbb4f22058bbe23fa41186d4a9d20a38498
Caio Andrade <caioertai@gmail.com>
2018-10-25 17:43:22 -0500

    Created tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_150927) do
+ActiveRecord::Schema.define(version: 2018_10_25_224154) do
 
   create_table "gardens", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2018_05_15_150927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["garden_id"], name: "index_plants_on_garden_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
