bd0e26f6df3c09e6ab2457e90a3c015e9649be9d
Caio Andrade <caioertai@gmail.com>
2018-10-25 19:49:53 -0500

    Added plant tagging with select2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :gardens do
     resources :plants, only: :create
   end
-  resources :plants, only: :destroy
+
+  resources :plants, only: :destroy do
+    # resources :plant_tags, only: [:new, :create] # < ----- Same deal as bellow
+    resources :plant_tags, only: %i[new create]
+  end
 
   root to: 'gardens#index'
 end
